1e9852628b2d1b25d8f116a068287a5772d60472
Bence Kiss <kiss.bence.balint@gmail.com>
2017-02-11 17:34:42 -0500

    Added format.json render json: @books to books controller.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,16 +9,17 @@ class BooksController < ApplicationController
       format.csv do
         render plain: Book.generate_csv(@books)
       end
-      # format.json do
-      #   formatted_data = @books.map do |book|
-      #     {
-      #       title: book.title,
-      #       author: book.author,
-      #       read_already: book.already_read
-      #     }
-      #   end
-      #   render json: formatted_data
-      # end
+      format.json do
+        # formatted_data = @books.map do |book|
+        #   {
+        #     title: book.title,
+        #     author: book.author,
+        #     read_already: book.already_read
+        #   }
+        # end
+        # render json: formatted_data
+        render json: @books
+      end
     end
   end
 
