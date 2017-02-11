bf89550a561d4bdde9bb2b8d0581dc674b76a5b2
Bence Kiss <kiss.bence.balint@gmail.com>
2017-02-11 17:18:24 -0500

    Added respond_to to the books controller to respond to html format.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,22 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
-    #code
+
+    respond_to do |format|
+      format.html
+      # format.text
+      # format.json do
+      #   formatted_data = @books.map do |book|
+      #     {
+      #       title: book.title,
+      #       author: book.author,
+      #       read_already: book.already_read
+      #     }
+      #   end
+      #   render json: formatted_data
+      # end
+    end
   end
+
+
 end
